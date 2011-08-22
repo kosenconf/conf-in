09c584ea7ed4f891f43b6336397ddee929ec2ba6
Hayato OKUMOTO <falcon@falconsrv.net>
2011-08-22 19:23:45 +0900

    devise設定:timeoutableを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :timeoutable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
--- db/migrate/20110815133922_devise_create_users.rb
+++ db/migrate/20110815133922_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.recoverable
       t.rememberable
       t.trackable
-
+			t.timeoutable
       # t.encryptable
       t.confirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
