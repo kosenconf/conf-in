97fbbde629d82a67f71577cbbf7afb19e7618998
Hayato OKUMOTO <falcon@falconsrv.net>
2011-08-24 21:46:10 +0900

    ・Twitterアイコンを表示するタグを生成するヘルパーを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  SIZE = {
+      mini: 'mini',
+      normal: 'normal',
+      bigger: 'bigger',
+      default: ''
+  }
+
+  # ページタイトルの連結
 	def title
 		if @page_title.nil? then
 			$SYSTEM_TITLE
@@ -6,8 +14,18 @@ module ApplicationHelper
 			"#{@page_title} | #{$SYSTEM_TITLE}"
 		end
 	end
-	
+
+	# QRコードの生成
 	def qrimg_tag(str)
 	  image_tag "http://chart.apis.google.com/chart?chs=150x150&cht=qr&chl=#{CGI.escape(str)}"
+	end
+
+	# Twitterアイコンの表示
+	def twicon_tag(id, size=:default)
+	  unless id.blank?
+	   image_tag "http://api.dan.co.jp/twicon/#{id}/#{SIZE[size]}"
+	  else
+	    image_tag( size == :bigger ? "73.png" : "24.png" )
+	  end
 	end
 end
