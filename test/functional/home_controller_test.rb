d6576a81ffacb696a867b9039db1a28622ac4d35
Hayato OKUMOTO <falcon@falconsrv.net>
2011-08-24 18:15:46 +0900

    userの機能テストを修正 homeの機能テストを修正 userの単体テストを修正

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -5,6 +5,7 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
 	# 正しいレスポンスの検証
 	test "should get index" do
     get :index
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,20 +1,27 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
   setup do
     @user = users(:one)
-  end
 
+  end
+# 使用しない
+=begin
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:users)
   end
-
+=end
+# 使用しない
+=begin
   test "should get new" do
     get :new
     assert_response :success
   end
+=end
+
 # 使用しない
 =begin
   test "should create user" do
@@ -32,17 +39,24 @@ class UsersControllerTest < ActionController::TestCase
     get :show, id: @user.to_param
     assert_response :success
   end
-
+# 使用しない
+=begin
   test "should get edit" do
     get :edit, id: @user.to_param
     assert_response :success
   end
+=end
 
-  test "should update user" do
+# 使用しない
+=begin
+  test "should not update not_authorized_user" do
     put :update, id: @user.to_param, user: @user.attributes
     assert_redirected_to user_path(assigns(:user))
   end
+=end
 
+# 使用しない
+=begin
   test "should destroy user" do
     assert_difference('User.count', -1) do
       delete :destroy, id: @user.to_param
@@ -50,4 +64,6 @@ class UsersControllerTest < ActionController::TestCase
 
     assert_redirected_to users_path
   end
+=end
+
 end
