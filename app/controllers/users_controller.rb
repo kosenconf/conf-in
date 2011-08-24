f1fe9d526a16b331e1819679b4f2b12b731053ba
Hayato OKUMOTO <falcon@falconsrv.net>
2011-08-24 22:15:04 +0900

    user#profileからuser#updateでのエラー時のリダイレクト先がおかしかったため修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -77,14 +77,10 @@ class UsersController < ApplicationController
   def update
     @user = current_user
 
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update_attributes(params[:user])
+      redirect_to(@user, :notice => 'ユーザ情報が更新されました。')
+    else
+      render :action => "profile"
     end
   end
 
