6d7dfa134258451d31c725e8970b9c4003908f7e
Hayato OKUMOTO <falcon@falconsrv.net>
2011-08-25 13:30:28 +0900

    users#profileの廃止と共に，users_controllerの整理 users#showのみにした それに伴いルーティングも変更 profileのViewの廃止

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,16 @@
 ConfInProcon::Application.routes.draw do
-
 	# root
 	root to: "home#index"
-	
-	#resources :users, only: [:index, :show, :update]
-	# get "users/:id"の後に置かないこと
-	
-	
-  resources :users do
-    get :profile, on: :collection
-  end
-	# for devise
-	devise_for :user, controllers: {registrations: "user/registrations"}
-	#get "/users/profile", to: "users#profile"
-	#get "users/:id", to: "users#show", as: :user_root
+  
+  # Userリソース
+  resources :users, only: [ :show ]
 
-	
+	# deviseの設定
+	devise_for :user
+  
+  # Eventリソース
 	resources :events do
+	  # Entryリソース
 		resources :entries do
 			collection do
 				get :xml
@@ -26,6 +20,4 @@ ConfInProcon::Application.routes.draw do
 			end
 		end
 	end
-	
-
 end
