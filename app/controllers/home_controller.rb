381ea1c09218ec512185cefc9bfe339c4f91ab11
Hayato OKUMOTO <falcon@falconsrv.net>
2011-09-22 23:34:23 +0900

    バグ fixes #54 : トップページのイベント通知が無効になっている 表題の件について修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 class HomeController < ApplicationController
   def index
+    #向こう１週間で参加登録が終了するイベント
+    @near_closing_events = Event.find(
+      :all,
+      :order => "joinable_period_end DESC",
+      :conditions => ["joinable_period_end <= ? AND joinable_period_end >= ?", 7.day.since, Time.now.utc ],
+      :limit => 5 #5件
+    )
+
+		#向こう２週間に開催されるイベント
+    @near_events = Event.find(
+      :all,
+      :order => "date DESC", #降順で
+      :conditions => ["date <= ? AND date >= ?", 14.day.since, Time.now.utc],
+      :limit => 5 #5件
+    )
 	end
 	
 	def about
