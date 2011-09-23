2bc8711bd352930197fc9230e630dc81d5c7bdaf
Hayato OKUMOTO <falcon@falconsrv.net>
2011-09-23 19:26:44 +0900

    ログインタイムアウトを廃止

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :timeoutable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 
   #参加イベントエントリ
   has_many :entries,
