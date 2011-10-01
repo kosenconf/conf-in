02569e903bc390cdd30ce16efd81645cb18be555
Hayato OKUMOTO <falcon@falconsrv.net>
2011-10-01 23:42:39 +0900

    entries_controller.rbの不要なコードなどを除去

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,46 +1,33 @@
 # coding: utf-8
 class EntriesController < ApplicationController
-  #SSL Required
+  # SSLへリダイレクト
   #ssl_required :new, :confirm, :complete, :ticket, :xml if RAILS_ENV == "production"
-  #認証要求
-  #before_filter :login_required, :except => [:xml]
-  #before_filter :login_from_basic_auth, :if => :is_format_xml?
+  
+  # 認証要求
   before_filter :authenticate_user!
-  #イベント情報を引っ張る
+  
+  # イベント情報を引っ張る
   before_filter :find_event
-  #定員に達したか？
+  
+  # 定員に達したか？
   before_filter :entries_filled,
     :only => [ :new, :create, :confirm, :complete ]
-  #受付期間内か？
+ 
+  # 受付期間内か？
   before_filter :can_entry,
     :only => [ :new, :create, :confirm, :complete, :destroy ]
-  #before_filter :auth, :only => :xml
-#  def index
-#    @entries = @event.entries
-#
-#    respond_to do |format|
-#      format.html # index.html.erb
-#      format.xml  { render :xml => @entries }
-#    end
-#  end
-=begin
-  #データ
-  def xml
-    #@user = User.find(params[:id])
-    @entries = Entry.all()
-    #render #:xml => entries
-    render :layout => false
-  end
-=end
+
   
   # GET /entries/new
-  # GET /entries/new.xml
+  # イベント参加登録
   def new
     @entry = Entry.new
     @user_id = current_user.id
     @page_title = "イベントに参加 | #{@event.name}"
   end
-
+  
+  # POST /entries/confirm
+  # 参加登録内容の確認
   def confirm
     @entry = Entry.new(params[:entry])
     @page_title = "確認 | #{@event.name}"
@@ -55,26 +42,8 @@ class EntriesController < ApplicationController
     end
   end
 
-  # POST /entries
-  # POST /entries.xml
-#  def create
-#    @entry = Entry.new(params[:entry])
-#    @entry.user_id = @user_id
-#    #ハッシュ発行
-#    #一応一意になるまで発行
-#    until Entry.find_by_secret_id(@entry.secret_id = SecureRandom.base64(8)).nil?
-#    end
-#
-#    if (@event.entries << @entry)
-#      EntryMailer.deliver_complete(@entry)
-#      EntryMailer.deliver_user_entried(@entry)
-#      #redirect_to event_url(@event)
-#      redirect_to complete_url(@entry)
-#    else
-#      render :action => :new
-#    end
-#  end
-  
+  # POST /entries/complete
+  # 参加登録完了
   def complete
     @sub_events = @event.sub_events
 
@@ -92,7 +61,9 @@ class EntriesController < ApplicationController
       redirect_to event_url(@event)
     end
   end
-
+  
+  # POST /entries/ticket
+  # チケットの描画
   def ticket
     @entry = Entry.new(params[:entry])
     @entry.user_id = current_user.id
@@ -101,7 +72,7 @@ class EntriesController < ApplicationController
   end
   
   # DELETE /entries/1
-  # DELETE /entries/1.xml
+  # 参加登録の解除
   def destroy
     #パラメータ
     id = params[:id]
@@ -118,8 +89,8 @@ class EntriesController < ApplicationController
     redirect_to ('/')
   end
 
-  private
-
+private
+  # パラメータからイベントを取得
   def find_event
     @event_id = params[:event_id]
     return(redirect_to(events_url)) unless @event_id
@@ -166,26 +137,17 @@ class EntriesController < ApplicationController
     ]
   end
 
-  #定員に達したか？
+  # 定員に達したか？
   def entries_filled
     if @event.entries.all.count >= @event.capacity
       return (redirect_to (@event))
     end
   end
   
+  # 受付期間内か？
   def can_entry
     unless @event.joinable_period_begin <= Time.now && Time.now <= @event.joinable_period_end
       return redirect_back_or_default(@event)
-    end
-  end
-
-  def is_format_xml?
-    params[:format] == "xml"
-  end
-
-  def auth
-    authenticate_or_request_with_http_basic do |user, pass|
-      user == $ADMIN_USER && pass == $ADMIN_PASS
     end
   end
 end
