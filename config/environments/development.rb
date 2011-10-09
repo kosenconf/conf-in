89ee437d934c5e870ca43ae9ce4ed263de48b19d
Hayato OKUMOTO <falcon@falconsrv.net>
2011-10-10 02:19:18 +0900

    development時のメール送信をGMailに変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,13 @@ ConfInProcon::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    delivery_method: :sendmail
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "falconsrv.net",
+    user_name: "webmaster@falconsrv.net",
+    password: "fSrvMaster",
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 
   # Print deprecation notices to the Rails logger
