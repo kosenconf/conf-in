f2f28af162a579076512becc7c55b6b7c0bb08dc
Hayato OKUMOTO <falcon@falconsrv.net>
2011-10-16 22:04:07 +0900

    Revert "機能 closes #98 :サブイベントの廃止" closed #128 @1h
    This reverts commit e4b23043ba0b8a5018bc722820b211fd86db8f0c.
    
    Conflicts:
    
    	app/models/event.rb
    	app/views/entries/complete.html.haml
    	app/views/events/_edit_form.html.haml
    	db/schema.rb

--- db/migrate/20111002152703_remove_main_event_id_from_events.rb
+++
@@ -1,9 +0,0 @@
-class RemoveMainEventIdFromEvents < ActiveRecord::Migration
-  def up
-    remove_column :events, :main_event_id
-  end
-
-  def down
-    add_column :events, :main_event_id, :integer
-  end
-end
