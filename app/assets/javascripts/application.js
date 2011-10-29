f3a6a4002512ee9eb7ced001696742017cbee5a3
Hayato OKUMOTO <falcon@falconsrv.net>
2011-10-29 22:46:01 +0900

    Bugs fixed #143 イベント新規作成ページのJavaScriptが動かない And precompiled assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,14 +3,15 @@
 //= require_tree .
 
 function remove_fields(link) {
-  $(link).previous("input[type=hidden]").value = "1";
-  $(link).up(".fields").hide();
+  $(link).prev(":hidden").val("1");
+  $(link).parents(".fields").hide();
 }
 
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
-  $(link).up().insert({
-    before: content.replace(regexp, new_id)
-  });
+  $(link).parent().before(
+    content.replace(regexp, new_id)
+  );
 }
+
