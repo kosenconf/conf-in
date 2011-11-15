ca4a0ca3bab5fdd4e92c2a35f10257fb0c83c86e
Hayato OKUMOTO <falcon@falconsrv.net>
2011-11-16 02:44:12 +0900

    機能 refs #158 : メールアドレス確認の廃止 ・メールアドレス確認廃止まで完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   #参加イベントエントリ
