f7477b37ed345c2316cba2eb3f41081a92d330f8
Hayato OKUMOTO <falcon@falconsrv.net>
2012-04-03 10:27:19 +0900

    add templates

---
+++ lib/templates/rails/controller/controller.rb
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+<% module_namespacing do -%>
+class <%= class_name %>Controller < ApplicationController
+<% actions.each do |action| -%>
+  def <%= action %>
+  end
+<%= "\n" unless action == actions.last -%>
+<% end -%>
+end
+<% end -%>
---
+++ lib/templates/rails/helper/helper.rb
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+<% module_namespacing do -%>
+module <%= class_name %>Helper
+end
+<% end -%>
